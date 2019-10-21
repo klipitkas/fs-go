6e2e3b0427c10b6859e69b24c95fbaf0ca53bb2a
Agustin Houlgrave <a.houlgrave@gmail.com>
2019-10-21 13:44:53 +0200

    changes

--- main.go
+++ main.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 )
 
-var port int
-var dir string
+var (
+	addr = flag.String("addr", ":8080", "The address that the server will listen to.")
+	dir  = flag.String("dir", ".", "The root directory that will be served.")
+)
 
 func main() {
-
-	// Handle the flags that are provided.
-	flag.IntVar(&port, "port", 8080, "The port that the server will listen to.")
-	flag.StringVar(&dir, "dir", ".", "The root directory that will be served.")
 	// Parse the flags.
 	flag.Parse()
 
 	// Create the fileserver.
-	fs := http.FileServer(http.Dir(dir))
+	fs := http.FileServer(http.Dir(*dir))
 
 	// Print details to the console.
-	log.Printf("FS - Port: %v | Dir: %v", port, dir)
+	log.Printf("FS - Address: %s | Dir: %s", *addr, *dir)
 
 	// Start the file server.
-	if err := http.ListenAndServe(":"+fmt.Sprintf("%d", port), fs); err != nil {
-		log.Fatalf("server listen on port %v: %v", port, err)
-	}
-
+	log.Fatal(http.ListenAndServe(*addr, fs))
 }
